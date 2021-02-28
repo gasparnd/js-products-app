15878e554451b029c710bb249cf2d4079ac9ca0f
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-02-28 13:23:46 -0300

    delete product

--- src/App.js
+++ src/App.js
@@ -14,24 +14,42 @@ class UI {
 		productUI.innerHTML = `
 			<div class="Hero__products-product">
 				<p><strong>Name:</strong> ${product.productName}, <strong>Price:</strong> $${product.productPrice}, <strong>Year:</strong> ${product.productYear}</p>
-				<button id="delete-product">Delete</button>
+				<button class="Hero__products-product-button" id="delete-product">Delete</button>
 			</div>
 		`
 		spaceDOM.appendChild(productUI)
 
 		this.resetForm()
+		this.showMessage('added', 'add-message')
 	}
 
 	resetForm() {
 		document.getElementById('product-form').reset()
 	}
 
-	removeProduct() {
-
+	removeProduct(element) {
+		if(element.id === 'delete-product') {
+			element.parentElement.parentElement.remove()
+			this.showMessage('removed', 'remove-message')
+		}
 	}
 	
-	showMessage() {
+	showMessage(message, className) {
+		const dom = document.querySelector('.Hero')
+		const app = document.querySelector('.Hero__app')
+		const alert = document.createElement('div')
+		alert.className = 'alert'
+		alert.innerHTML = `
+			<div class="${className}">
+				<p>The product was ${message} successfully</p>
+			</div>
+		`
 
+		dom.insertBefore(alert, app)
+
+		setTimeout(() => {
+			alert.remove()
+		},1500)
 	}	
 	
 }
@@ -43,10 +61,18 @@ document.getElementById('product-form').addEventListener('submit', ev => {
 	const productPrice = document.getElementById('product-price').value
 	const productYear = document.getElementById('product-year').value
 
-	const product = new Product({name: productName, price: productPrice, year: productYear})
 
+	if(productName === '' || productPrice === '' || productYear === '') {
+		alert('Please complete all the cells')
+	} else {
+		const product = new Product({name: productName, price: productPrice, year: productYear})
+
+		const ui = new UI()
+		ui.addProduct(product)	
+	}
+})
+
+document.getElementById('product-list').addEventListener('click', (ev) => {
 	const ui = new UI()
-	ui.addProduct(product)	
-
-	console.log(product)
+	ui.removeProduct(ev.target)
 })
